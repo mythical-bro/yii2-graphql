9f0ed51e0ce1c7cc67a57c3ee70b87d878e457c3
Māris Graudiņš <maris.graudins91@gmail.com>
2019-02-13 17:39:05 +0200

    authorization support

--- GraphQLField.php
+++ GraphQLField.php
@@ -5,6 +5,7 @@ namespace mgcode\graphql;
 use GraphQL\Type\Definition\Type;
 use yii\base\BaseObject;
 use yii\base\InvalidConfigException;
+use yii\web\ForbiddenHttpException;
 
 abstract class GraphQLField extends BaseObject
 {
@@ -22,6 +23,24 @@ abstract class GraphQLField extends BaseObject
         return [];
     }
 
+    protected function getResolver()
+    {
+        if (!method_exists($this, 'resolve')) {
+            return null;
+        }
+        $resolver = [$this, 'resolve'];
+        $authorize = [$this, 'authorize'];
+
+        return function () use ($resolver, $authorize) {
+            $arguments = func_get_args();
+            // Authorize request
+            if (method_exists($this, 'authorize') && call_user_func($authorize, $arguments) !== true) {
+                throw new ForbiddenHttpException('You are not allowed to perform this action.');
+            }
+            return call_user_func_array($resolver, $arguments);
+        };
+    }
+
     /**
      * Convert instance to an array.
      * @return array
@@ -31,12 +50,16 @@ abstract class GraphQLField extends BaseObject
         if (!method_exists($this, 'resolve')) {
             throw new InvalidConfigException('Resolve callback is not defined');
         }
+
         $attributes = array_merge([
             'description' => $this->description(),
             'type' => $this->type(),
             'args' => $this->args(),
-            'resolve' => [$this, 'resolve'],
         ], $this->attributes());
+
+        if ($resolver = $this->getResolver()) {
+            $attributes['resolve'] = $resolver;
+        }
         return $attributes;
     }
 }
